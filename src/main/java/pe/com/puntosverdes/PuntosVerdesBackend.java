b007fe32f62a4d74b0491f21f5527e95f78b8672
gamma <santiagogamarrasanchez@gmail.com>
2025-09-25 23:36:22 -0500

    rename main

--- src/main/java/pe/com/puntosverdes/PuntosVerdesBackend.java
+++ src/main/java/pe/com/puntosverdes/PuntosVerdesBackend.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PuntosVerdesBackendApplication {
-//
+public class PuntosVerdesBackend {
+
 	public static void main(String[] args) {
-		SpringApplication.run(PuntosVerdesBackendApplication.class, args);
+		SpringApplication.run(PuntosVerdesBackend.class, args);
 	}
 
 }
