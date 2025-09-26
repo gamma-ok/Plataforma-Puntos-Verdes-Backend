8707e14f03ed0af4be452f76135d5afe0890cf9f
gamma <santiagogamarrasanchez@gmail.com>
2025-09-25 23:29:59 -0500

    .

--- src/main/java/pe/com/puntosverdes/PuntosVerdesBackendApplication.java
+++ src/main/java/pe/com/puntosverdes/PuntosVerdesBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PuntosVerdesBackendApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(PuntosVerdesBackendApplication.class, args);
 	}
